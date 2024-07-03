a2380085c656863a89a8f2214fa0dd9c919324d9
Tuo Chen <potato008@gmail.com>
2024-07-03 17:57:25 +0900

    Added new files

---
+++ Processor/Src/Verification/TestCode/C/MDPBenchmark/mdpb.c
@@ -0,0 +1,20 @@
+#include "../lib.c"
+
+__attribute__((noinline)) int test(volatile int* a, volatile int* b, int n)
+{
+    int j = 0;
+    for (int i = 0; i < n; i++) {
+        *a = i;
+        j += *b;
+    }
+    return j;
+}
+
+int x = 0;
+int y = 0;
+
+int main(){
+    test(&x, &x, 1000);
+    // test(&x, &y, 1000);
+    return 0;
+}
