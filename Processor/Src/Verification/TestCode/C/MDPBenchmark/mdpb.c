62cf37505d16da451d0fe4ae0a4ee5f7e9126432
Tuo Chen <potato008@gmail.com>
2024-07-04 15:07:56 +0900

    Improved MDPBenchmark program

--- Processor/Src/Verification/TestCode/C/MDPBenchmark/mdpb.c
+++ Processor/Src/Verification/TestCode/C/MDPBenchmark/mdpb.c
@@ -4,11 +4,11 @@ __attribute__((noinline)) int test(volatile int* a, volatile int* b, int n)
 {
     int j = 0;
     for (int i = 0; i < n; i++) {
-        *a = i;
+        *a = i/2+i+1;
         j += *b;
     }
     return j;
-}
+} 
 
 int x = 0;
 int y = 0;
@@ -16,5 +16,5 @@ int y = 0;
 int main(){
     test(&x, &x, 1000);
     // test(&x, &y, 1000);
-    return 0;
+    return 0; 
 }
